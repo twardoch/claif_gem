720bae3cc453cc9e2ab18dd2a1145ea33eb7e7b5
Adam Twardoch <adam+github@twardoch.com>
2025-07-01 17:32:47 +0200

    Initial commit

---
+++ src/claif_gem/__init__.py
@@ -0,0 +1,5 @@
+"""claif gem plugin"""
+
+from importlib import metadata
+
+__version__ = metadata.version(__name__) 
---
+++ tests/test_package.py
@@ -0,0 +1,6 @@
+"""Test suite for claif_gem."""
+
+def test_version():
+    """Verify package exposes version."""
+    import claif_gem
+    assert claif_gem.__version__
